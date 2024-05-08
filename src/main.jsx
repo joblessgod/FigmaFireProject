ec17cb8aa260b9b0083dbedadbc1943bf657f8a5
JobLessGod <iamjoblessgod@gmail.com>
2024-05-09 01:32:20 +0545

    path fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
 import "react-loading-skeleton/dist/skeleton.css";
+import { ToastContainer } from "react-toastify";
+
 import store from "./redux/store.js";
 import { Provider } from "react-redux";
 
